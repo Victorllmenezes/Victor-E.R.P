bd2ce52cdf20018e7f74c4616f54caf125905e48
Joao <jvma@MacBook-Air-de-Joao-Victor.local>
2021-08-20 19:31:18 -0300

    Implemented part of the Product class

---
+++ Produto.js
@@ -0,0 +1,8 @@
+class Produto {
+  constructor(codigo, descritivo, valorUnitarioCompra, valorUnitarioVenda) {
+    this.codigo = codigo;
+    this.descritivo = descritivo;
+    this.valorUnitarioCompra = valorUnitarioCompra;
+    this.valoUnitarioVenda = valorUnitarioVenda;
+  }
+}
