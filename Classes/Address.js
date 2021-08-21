668711ebec3ebc050711c8d5bddf13382c67c1f5
Joao <jvma@MacBook-Air-de-Joao-Victor.local>
2021-08-21 16:11:18 -0300

    implemented address class

---
+++ Classes/Address.js
@@ -0,0 +1,10 @@
+class Address {
+  constructor(street, number, state, city, neighborhood, zipCode) {
+    this.street = street;
+    this.number = number;
+    this.state = state;
+    this.city = city;
+    this.neighborhood = neighborhood;
+    this.zipCode = zipCode;
+  }
+}
