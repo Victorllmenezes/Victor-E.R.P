851a3bcb19e904f00e8157eff77ebb5037e19268
Joao <jvma@MacBook-Air-de-Joao-Victor.local>
2021-08-21 09:56:49 -0300

    Implemented product and productItem classes and some useful price/cost methods

--- Produto.js
+++ Produto.js
@@ -1,8 +1,36 @@
-class Produto {
-  constructor(codigo, descritivo, valorUnitarioCompra, valorUnitarioVenda) {
-    this.codigo = codigo;
-    this.descritivo = descritivo;
-    this.valorUnitarioCompra = valorUnitarioCompra;
-    this.valoUnitarioVenda = valorUnitarioVenda;
+class Product {
+  constructor(id, description, purchasePrice, standardProfitMargin) {
+    this.id = id;
+    this.description = description;
+    this.purchasePrice = purchasePrice;
+    this.standardProfitMargin = standardProfitMargin;
+  }
+}
+
+class ProductItem extends Product {
+  constructor(
+    id,
+    description,
+    purchasePrice,
+    standardProfitMargin,
+    quantity,
+    additionalCost,
+    taxes,
+    sellingPrice
+  ) {
+    super(id, description, purchasePrice, standardProfitMargin);
+    this.quantity = quantity;
+    this.additionalCost = additionalCost;
+    this.taxes = taxes;
+    this.sellingPrice = sellingPrice;
+  }
+  sellingCost() {
+    return (
+      this.purchasePrice * this.quantity + this.additionalCost + this.taxes
+    );
+  }
+
+  lucro() {
+    return this.sellingPrice - sellingCost();
   }
 }
